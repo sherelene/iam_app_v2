ca78051d48ad7648b1fce55e0e65ac8b602b89fe
sherelene <sherelenedebelen@gmail.com>
2023-04-04 23:42:17 -0700

    Initial commit. downloaded dependencies and started firebase

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final isLoggedIn = FirebaseAuth.instance.currentUser != null;
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Fitness',
+      theme: ThemeData(
+        textTheme:
+        TextTheme(bodyText1: TextStyle(color: ColorConstants.textColor)),
+        fontFamily: 'NotoSansKR',
+        scaffoldBackgroundColor: Colors.white,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: isLoggedIn ? TabBarPage() : OnboardingPage(),
+    );
+  }
+}
