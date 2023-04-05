54d9ee9af279ead281358223bce11b5ab603cd99
sherelene <sherelenedebelen@gmail.com>
2023-04-05 11:31:44 -0700

    added ios and android sdk in appDelegate.swift and both build.grade

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,4 +1,4 @@
-import UIKit
+/* import UIKit
 import Flutter
 
 @UIApplicationMain
@@ -9,5 +9,23 @@ import Flutter
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}  */
+
+import UIKit
+import FirebaseCore
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+      [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+
+    return true
   }
 }
